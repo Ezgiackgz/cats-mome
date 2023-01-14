49c88de6a439e85413f212acaf0052782774b238
Ezgiackgz <ezgisuackgz@gmail.com>
2023-01-15 01:13:12 +0300

    new-user add and edit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
+import {createBrowserRouter, RouterProvider,} from "react-router-dom";
+import Root, {loader as rootLoader, action as rootAction} from "./routes/route";
+import Error from  "./error-page";
+import Contact,{loader as contactLoader} from "./routes/contact";
+import Edit, {action as editAction} from "./routes/edit";
 
 const router = createBrowserRouter([
     {
         path:"/",
-        element: <div>Hello World</div>
-    }
+        element:<Root />,
+        errorElement:<Error />,
+        loader:rootLoader,
+        action:rootAction,
+        children:[
+            {
+                path: "contacts/:contactId",
+                element: <Contact />,
+                loader:contactLoader,
+            },
+            {
+                path:"contacts/:contactId/edit",
+                element:<Edit />,
+                loader:contactLoader,
+                action:editAction
+            }
+        ]
+    },
+
 ])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
---
+++ src/routes/route.js
@@ -0,0 +1,75 @@
+import {Outlet ,useLoaderData, Form , NavLink} from "react-router-dom";
+import {getContacts , createContact} from "../contacts";
+
+
+export async function loader() {
+    const contacts = await getContacts();
+    return { contacts };
+}
+export async function action() {
+    const contact = await createContact();
+    return { contact };
+}
+
+export default function Root() {
+    const {contacts} = useLoaderData();
+    return (
+        <>
+            <div id="sidebar">
+                <h1>Cats Mome</h1>
+                <div>
+                    <form id="search-form" role="search">
+                        <input id="q" aria-label="Search contacts" placeholder="Search" type="search" name="q"/>
+                        <div id="search-spinner" aria-hidden hidden={true}/>
+                        <div className="sr-only" aria-live="polite"></div>
+                    </form>
+                    <div>
+                        <Form method="post">
+                            <button type="submit">Ekle</button>
+                        </Form>
+
+                    </div>
+
+                </div>
+                <nav>
+                    {contacts.length ? (
+                        <ul>
+                            {contacts.map((contact) => (
+                                <li key={contact.id}>
+                                    <NavLink to={`contacts/${contact.id}`}  className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""}>
+                                        {contact.first || contact.last ? (
+                                            <>
+                                                {contact.first} {contact.last}
+                                            </>
+                                        ) : (
+                                            <i>No Name</i>
+                                        )}{" "}
+                                        {contact.favorite && <span>★</span>}
+                                    </NavLink>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>
+                            <i>No contacts</i>
+                        </p>
+                    )}
+                    { /*
+                        <ul>
+                            <li>
+                                <Link to={`/contacts/1`}>Your Name</Link>
+                            </li>
+                            <li>
+                                <Link to={`/contacts/2`}>Your Name</Link>
+                            </li>
+                        </ul>
+                   */ }
+
+                </nav>
+            </div>
+            <div id="detail">
+                <Outlet />
+            </div>
+        </>
+    );
+}
